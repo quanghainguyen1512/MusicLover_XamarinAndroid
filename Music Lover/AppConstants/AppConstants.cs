1f45e153055bcdabe920f4316b6a459c5f37334a
Quang Hai <quanghai.ng@outlook.com.vn>
2017-12-13 07:28:47 +0700

    Create AppConstants

---
+++ Music Lover/AppConstants/AppConstants.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Music_Lover.AppConstants
+{
+    public class AppConstants
+    {
+        public static string NAVIGATE_LIBRARY = "navigate_library";
+        public static string NAVIGATE_PLAYLIST = "navigate_playlist";
+        public static string NAVIGATE_NOWPLAYING = "navigate_nowplaying";
+        public static string NAVIGATE_ALBUM = "navigate_album";
+        public static string NAVIGATE_QUEUE = "navigate_queue";
+        public static string NAVIGATE_ARTIST = "navigate_artist";
+    }
+}
