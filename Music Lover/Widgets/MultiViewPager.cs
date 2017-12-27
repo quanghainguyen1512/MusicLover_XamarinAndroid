ac6e18b63941ccf81347fc50774041e9a80ac75c
Quang Hai <quanghai.ng@outlook.com.vn>
2017-12-27 22:19:14 +0700

    Added Widgets

--- Music Lover/Widgets/MultiViewPager.cs
+++ Music Lover/Widgets/MultiViewPager.cs
@@ -1,17 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
 using Android.Content;
 using Android.Graphics;
-using Android.OS;
 using Android.Runtime;
 using Android.Support.V4.View;
 using Android.Util;
 using Android.Views;
-using Android.Widget;
 using Java.Lang;
 using Math = System.Math;
 
