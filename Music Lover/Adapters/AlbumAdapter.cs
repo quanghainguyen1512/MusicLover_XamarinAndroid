e20b815d86b4992ce3a872459544b1392d5fe29f
Quang Hai <quanghai.ng@outlook.com.vn>
2018-01-02 01:26:17 +0700

    Album handler

---
+++ Music Lover/Adapters/AlbumAdapter.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.Graphics;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+using Com.Nostra13.Universalimageloader.Core;
+using Com.Nostra13.Universalimageloader.Core.Assist;
+using Com.Nostra13.Universalimageloader.Core.Display;
+using Com.Nostra13.Universalimageloader.Core.Listener;
+using Music_Lover.Models;
+using Music_Lover.Utils;
+
+namespace Music_Lover.Adapters
+{
+    public class AlbumAdapter : RecyclerView.Adapter, Palette.IPaletteAsyncListener
+    {
+        private List<Album> _albums;
+        private Activity _context;
+        private bool _isGrid;
+        private AlbumItemHolder _itemHolder;
+
+        public AlbumAdapter(Activity context, List<Album> albums)
+        {
+            _albums = albums;
+            _context = context;
+        }
+
+        public override int ItemCount => _albums != null ? _albums.Count : 0;
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            if (!(holder is AlbumItemHolder itemHolder))
+                return;
+            var localAlbum = _albums[position];
+
+            _itemHolder = itemHolder;
+            itemHolder.Title.Text = localAlbum.Title;
+            itemHolder.Artist.Text = localAlbum.ArtistName;
+
+            ImageLoader.Instance.DisplayImage(MusicUtils.GetAlbumArtUri(localAlbum.Id).ToString(), itemHolder.AlbumArt,
+                new DisplayImageOptions.Builder().CacheInMemory(true)
+                .ShowImageOnFail(Resource.Drawable.ic_empty_music2)
+                .ResetViewBeforeLoading(true)
+                .Displayer(new FadeInBitmapDisplayer(400))
+                .Build(), 
+                new ImageLoadingListener()
+                {
+                    LoadingComplete = (loadedImage) =>
+                    {
+                        if (_isGrid)
+                            new Palette.Builder(loadedImage).Generate(this);
+                    },
+                    LoadingFailed = () =>
+                    {
+                        if (_isGrid)
+                        {
+                            itemHolder.Footer.SetBackgroundColor(Color.White);
+                            if (_context != null)
+                            {
+                                var textColor = new Color(MusicUtils.GetTextColorPrimary(_context));
+                                itemHolder.Title.SetTextColor(textColor);
+                                itemHolder.Artist.SetTextColor(textColor);
+                            }
+                        }
+                    }
+                }
+            );
+        }
+
+        public void UpdateData(List<Album> list)
+        {
+            _albums = list;
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            var view = _isGrid
+                ? LayoutInflater.From(parent.Context).Inflate(Resource.Layout.item_album_grid, null)
+                : LayoutInflater.From(parent.Context).Inflate(Resource.Layout.item_album_list, null);
+            return new AlbumItemHolder(view)
+            {
+                OnClickAction = (albumArt, adapterPos) =>
+                {
+                    NavigationUtils.NavigateToAlbum(_context, _albums[adapterPos].Id, new KeyValuePair<string, View>($"transition_album_art{adapterPos}", albumArt));
+                }
+            };
+        }
+
+        public void OnGenerated(Palette palette)
+        {
+            var swatch = palette.VibrantSwatch;
+            Color textColor = Color.White;
+            Color backgoundColor = Color.Black;
+            if (swatch != null)
+            {
+                backgoundColor = new Color(swatch.Rgb);
+            }
+            else
+            {
+                swatch = palette.MutedSwatch;
+                backgoundColor = new Color(swatch.Rgb);
+            }
+            _itemHolder.Footer.SetBackgroundColor(backgoundColor);
+            textColor = MusicUtils.GetSuitableTextColor(backgoundColor);
+            _itemHolder.Title.SetTextColor(textColor);
+            _itemHolder.Artist.SetTextColor(textColor);
+        }
+
+        private class AlbumItemHolder : RecyclerView.ViewHolder, View.IOnClickListener
+        {
+            public TextView Title { get; set; }
+            public TextView Artist { get; set; }
+            public ImageView AlbumArt { get; set; }
+            public View Footer { get; set; }
+
+            public Action<ImageView, int> OnClickAction { private get; set; }
+
+            public AlbumItemHolder(View itemView) : base(itemView)
+            {
+                Title = itemView.FindViewById<TextView>(Resource.Id.album_title);
+                Artist = itemView.FindViewById<TextView>(Resource.Id.album_artist);
+                AlbumArt = itemView.FindViewById<ImageView>(Resource.Id.album_art);
+                Footer = itemView.FindViewById(Resource.Id.footer);
+
+                itemView.SetOnClickListener(this);
+            }
+
+            public void OnClick(View v)
+            {
+                OnClickAction(AlbumArt, AdapterPosition);
+            }
+        }
+        
+        private class ImageLoadingListener : SimpleImageLoadingListener
+        {
+            public Action<Bitmap> LoadingComplete { get; set; }
+            public Action LoadingFailed { get; set; }
+
+            public override void OnLoadingComplete(string p0, View p1, Bitmap p2)
+            {
+                LoadingComplete(p2);
+            }
+            public override void OnLoadingFailed(string p0, View p1, FailReason p2)
+            {
+                LoadingFailed();
+            }
+        }
+    }
+}
--- Music Lover/AppFragments/AlbumFragment.cs
+++ Music Lover/AppFragments/AlbumFragment.cs
@@ -1,9 +1,214 @@
-﻿using Android.Support.V4.App;
+﻿using Android.Graphics;
+using Android.OS;
+using Android.Support.V4.App;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Music_Lover.Adapters;
+using Music_Lover.Loader;
+using Music_Lover.Utils;
+using Music_Lover.Widgets;
+using System;
+using System.Threading.Tasks;
 
 namespace Music_Lover.AppFragments
 {
     public class AlbumFragment : Fragment
     {
-        
+        private AlbumAdapter _adapter;
+        private RecyclerView _recyclerView;
+        private FastScroller _fastScoller;
+        private GridLayoutManager _gridLayoutManager;
+        private RecyclerView.ItemDecoration _decoration;
+        private PreferencesUtility _preferences;
+        private bool _isGrid;
+
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            _preferences = PreferencesUtility.GetInstance(Activity);
+            _isGrid = _preferences.IsAlbumInGrid();
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var rootView = inflater.Inflate(Resource.Layout.fragment_recyclerview, container, false);
+
+            _recyclerView = rootView.FindViewById<RecyclerView>(Resource.Id.recyclerview);
+            _fastScoller = rootView.FindViewById<FastScroller>(Resource.Id.fastscroller);
+
+            _gridLayoutManager = new GridLayoutManager(Activity, 2);
+            SetLayoutManager();
+
+            if (Activity != null)
+            {
+                Task.Run(async () =>
+                {
+                    await LoadAlbums();
+                });
+            }
+
+            return rootView;
+        }
+
+        public override void OnActivityCreated(Bundle savedInstanceState)
+        {
+            base.OnActivityCreated(savedInstanceState);
+            HasOptionsMenu = true;
+        }
+
+        public override void OnCreateOptionsMenu(IMenu menu, MenuInflater inflater)
+        {
+            base.OnCreateOptionsMenu(menu, inflater);
+            inflater.Inflate(Resource.Menu.album_sort_by, menu);
+            inflater.Inflate(Resource.Menu.menu_show_as, menu);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.menu_sort_by_az:
+                {
+                    Task.Run(async () =>
+                    {
+                        await _preferences.SetAlbumSortOrder(SortOrder.Album.ALBUM_A_Z);
+                        await ReloadAdapter();
+                    });
+                    return true;
+                }
+                case Resource.Id.menu_sort_by_za:
+                {
+                    Task.Run(async () =>
+                    {
+                        await _preferences.SetAlbumSortOrder(SortOrder.Album.ALBUM_Z_A);
+                        await ReloadAdapter();
+                    });
+                    return true;
+                }
+                case Resource.Id.menu_sort_by_artist:
+                {
+                    Task.Run(async () =>
+                    {
+                        await _preferences.SetAlbumSortOrder(SortOrder.Album.ALBUM_ARTIST);
+                        await ReloadAdapter();
+                    });
+                    return true;
+                }
+                case Resource.Id.menu_sort_by_year:
+                {
+                    Task.Run(async () =>
+                    {
+                        await _preferences.SetAlbumSortOrder(SortOrder.Album.ALBUM_YEAR);
+                        await ReloadAdapter();
+                    });
+                    return true;
+                }
+                case Resource.Id.menu_sort_by_number_of_songs:
+                {
+                    Task.Run(async () =>
+                    {
+                        await _preferences.SetAlbumSongSortOrder(SortOrder.Album.ALBUM_NUMBER_OF_SONGS);
+                        await ReloadAdapter();
+                    });
+                    return true;
+                }
+                case Resource.Id.menu_show_as_list:
+                {
+                    _isGrid = false;
+                    Task.Run(async () =>
+                    {
+                        await _preferences.SetAlbumInGrid(false);
+                        await UpdateLayoutManager(1);
+                    });
+                    return true;
+                }
+                case Resource.Id.menu_show_as_grid:
+                {
+                    _isGrid = true;
+                    Task.Run(async () =>
+                    {
+                        await _preferences.SetAlbumInGrid(true);
+                        await UpdateLayoutManager(2);
+                    });
+                    return true;
+                }
+            }
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private async Task LoadAlbums()
+        {
+            await Task.Run(() =>
+            {
+                if (Activity is null)
+                    return;
+                _adapter = new AlbumAdapter(Activity, AlbumLoader.GetAllAlbums(Activity));
+                SetItemDecoration();
+                _recyclerView.SetAdapter(_adapter);
+            });
+        }
+
+        private void SetLayoutManager()
+        {
+            if (_isGrid)
+            {
+                _gridLayoutManager.SpanCount = 2;
+                _fastScoller.Visibility = ViewStates.Gone;
+            }
+            else
+            {
+                _gridLayoutManager.SpanCount = 1;
+                _fastScoller.Visibility = ViewStates.Visible;
+                _fastScoller.SetRecyclerView(_recyclerView);
+            }
+            _recyclerView.SetLayoutManager(_gridLayoutManager);
+        }
+
+        private void SetItemDecoration()
+        {
+            if (_isGrid)
+            {
+                var space = Activity.Resources.GetDimensionPixelSize(Resource.Dimension.spacing_card_album_grid);
+                _decoration = new SpacesItemDecoration(space);
+            }
+            else
+                _decoration = new DividerItemDecoration(Activity, DividerItemDecoration.Vertical);
+            _recyclerView.AddItemDecoration(_decoration);
+        }
+
+        private async Task UpdateLayoutManager(int col)
+        {
+            SetLayoutManager();
+            _recyclerView.RemoveItemDecoration(_decoration);
+            await LoadAlbums();
+        }
+
+        private async Task ReloadAdapter()
+        {
+            await Task.Run(() => 
+            {
+                var list = AlbumLoader.GetAllAlbums(Activity);
+                _adapter.UpdateData(list);
+            });
+            _adapter.NotifyDataSetChanged();
+        }
+
+        public class SpacesItemDecoration : RecyclerView.ItemDecoration
+        {
+            private int _space;
+
+            public SpacesItemDecoration(int space)
+            {
+                _space = space;
+            }
+
+            public override void GetItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state)
+            {
+                outRect.Left = _space;
+                outRect.Top = _space;
+                outRect.Right = _space;
+                outRect.Bottom = _space;
+            }
+        }
     }
 }
